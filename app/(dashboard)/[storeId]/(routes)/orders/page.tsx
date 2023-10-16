569ea82d71607d85b3822270f99835d6ac7a0244
Sharvil-Karwa <karwasharvil@gmail.com>
2023-10-16 22:10:31 +0530

    first commit

---
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -0,0 +1,42 @@
+import { format } from "date-fns";
+
+import prismadb from "@/lib/prismadb";
+import { formatter } from "@/lib/utils";
+
+import { OrderColumn } from "./components/columns"
+import { OrderClient } from "./components/client";
+
+
+const OrdersPage = async ({
+  params
+}: {
+  params: { storeId: string }
+}) => {
+  const orders = await prismadb.order.findMany({
+    where: {
+      storeId: params.storeId
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  });
+
+  const formattedOrders: OrderColumn[] = orders.map((item) => ({
+    id: item.id,
+    customer: item.customer,
+    customer_email: item.customer_email,
+    product: item.product,
+    quantity: item.quantity,
+    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <OrderClient data={formattedOrders} />
+      </div>
+    </div>
+  );
+};
+
+export default OrdersPage;
